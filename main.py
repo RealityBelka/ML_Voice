9c8cba5ca02fe1228c56ed0f0315c3247d3fb5a0
Dmitry Panov <dpanov2302@gmail.com>
2024-10-12 11:56:11 +0300

    update readme & output format

--- main.py
+++ main.py
@@ -14,35 +14,35 @@ def process(audio_file, ap, example_numerals=None):
     noise_level = ap.analyze_audio_quality(audio_wav)
     # print("1-2) noise_level: ", noise_level)
     if noise_level == "тихо":
-        return "Говорите громче или переместитесь в более тихое место"
+        return flag, "Говорите громче или переместитесь в более тихое место"
     if noise_level == "громко":
-        return "Говорите тише или отодвиньте телефон от лица"
+        return flag, "Говорите тише или отодвиньте телефон от лица"
 
     recognized_text = ap.text_recognition(audio_wav)
 
     language = ap.check_language(recognized_text)
     # print("3) language: ", language)
     if language != "ru":
-        return "Произносите указанные цифры на русском языке"
+        return flag, "Произносите указанные цифры на русском языке"
 
     only_numerals = ap.only_numerals(recognized_text)
     # print("4) only_numerals: ", only_numerals)
     if not only_numerals:
-        return "Произносите только указанные на экране цифры"
+        return flag, "Произносите только указанные на экране цифры"
 
     check_single_speaker = ap.check_single_speaker(audio_wav)
     # print("5) check_single_speaker: ", check_single_speaker)
     if not check_single_speaker:
-        return "Посторонние шумы. Переместитесь в более тихое место"
+        return flag, "Посторонние шумы. Переместитесь в более тихое место"
 
-    result = [int(x) for x in recognized_text.split()]
+    recognized_numerals = [int(x) for x in recognized_text.split()]
 
-    if example_numerals is not None and result != example_numerals:
-        return "Произносите только указанные на экране цифры"
+    if example_numerals is not None and recognized_numerals != example_numerals:
+        return flag, "Произносите только указанные на экране цифры"
 
     flag = True
 
-    return "Запись прошла проверку успешно."
+    return flag, "Запись прошла проверку успешно."
 
 
 def main():
@@ -52,10 +52,11 @@ def main():
     audio_file = "audio_samples/ogg/overload_3.ogg"
     example_numerals = [1, 2, 3, 4, 8, 8]
 
-    message = process(audio_file, ap, example_numerals)
+    status, message = process(audio_file, ap, example_numerals)
 
-    print(message)
+    return {"status": status, "message": message}
 
 
 if __name__ == "__main__":
-    main()
+    result = main()
+    print(result)
